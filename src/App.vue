29cc84ab941d169a8faf1e0b5d0b385b5b44be23
KASHI002 <116870900+KASHI002@users.noreply.github.com>
2020-04-18 06:54:27 +0000

    Removed unnecessary line

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+
+<template>
+  <div class="container mx-auto flex flex-col items-center bg-gray-100 p-4">
+    <div class="container">
+      <div class="w-full my-4"></div>
+      <add-ticker @add-ticker="add" :disabled="tooManyTickersAdd" />
+      <template v-if="tickers.length">
+        <hr class="w-full border-t border-gray-600 my-4" />
+        <div>Filter:
