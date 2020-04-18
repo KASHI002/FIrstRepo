79e509640da84a59d692ce0a88b550aa5fb328bd
KASHI002 <116870900+KASHI002@users.noreply.github.com>
2020-04-18 17:18:48 +0000

    update unicom project.

--- src/App.vue
+++ src/App.vue
@@ -40,3 +40,27 @@
             back
           </button>
           <button
+              class="
+              my-4
+              inline-flex
+              items-center
+              mx-2
+              py-2
+              px-4
+              border border-transparent
+              shadow-sm
+              text-sm
+              leading-4
+              font-medium
+              rounded-full
+              text-white
+              bg-gray-600
+              hover:bg-gray-700
+              transition-colors
+              duration-300
+              focus:outline-none
+              focus:ring-2
+              focus:ring-offset-2
+              focus:ring-gray-500"
+
+              v-if="hasNextPage"
