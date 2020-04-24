0812fa25c4821c4af9680aa038f8955392d41b99
KASHI002 <116870900+KASHI002@users.noreply.github.com>
2020-04-24 13:23:57 +0000

    Fixed screenshot

--- src/App.vue
+++ src/App.vue
@@ -120,3 +120,24 @@
                 viewBox="0 0 20 20"
                 fill="#718096"
                 aria-hidden="true"
+              >
+                <path
+                  fill-rule="evenodd"
+                  d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z"
+                  clip-rule="evenodd"
+                ></path></svg
+              >Delete
+            </button>
+          </div>
+        </dl>
+        <hr class="w-full border-t border-gray-600 my-4" />
+      </template>
+      <ticker-graph v-if="selectedTicker" :ticker="selectedTicker" :graph="graph" @graph-reset="reset"></ticker-graph>
+    </div>
+  </div>
+</template>
+
+<script>
+import { subscribeToTicker, unsubscribeFromTicker } from "./api";
+import AddTicker from "./components/AddTicker";
+import TickerGraph from "./components/TickerGraph";
