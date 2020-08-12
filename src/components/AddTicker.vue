9c26ea64bf6fd762f4b666f7ac01492b17ff5d60
KASHI002 <116870900+KASHI002@users.noreply.github.com>
2020-08-12 08:28:35 +0000

    Update version to 1.0.1

--- src/components/AddTicker.vue
+++ src/components/AddTicker.vue
@@ -2,4 +2,4 @@
   <section>
     <div class="flex">
       <div class="max-w-xs">
-        <label for="wallet" class="block text-sm font-
+        <label for="wallet" class="block text-sm font-medium text-gray-700"
