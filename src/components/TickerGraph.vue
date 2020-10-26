0550a4bfb601610c27ae5f23c125ed2690bc5234
KASHI002 <116870900+KASHI002@users.noreply.github.com>
2020-10-26 11:47:17 +0000

    JavaDoc

--- src/components/TickerGraph.vue
+++ src/components/TickerGraph.vue
@@ -1,3 +1,15 @@
 
 <template>
   <section class="relative">
+    <h3 class="text-lg leading-6 font-medium text-gray-900 my-8">
+      {{ ticker.name }} - USD
+    </h3>
+    <div
+      class="flex items-end border-gray-600 border-b border-l h-64"
+      ref="graph"
+    >
+      <div
+        v-for="(bar, idx) in normalizedGraph"
+        :key="idx"
+        :style="{ height: `${bar}%` }"
+        class="bg-purple-800 border w-10"
